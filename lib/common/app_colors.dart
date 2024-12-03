976d844fc411fc222ae851649ba326aa6be5e27b
bavananthan <bavananthan14@gmail.com>
2024-12-03 16:49:29 +0530

    first commit after set file structure

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  Color primaryColor = Color(0xffffffff);
+  Color baseColor = Color(0Xff1CAE81);
+  Color darkGray = Color(0xffE8E8E8);
+  Color textColor = Color(0xff060606);
+  Color yellow = Color(0xffFFB200);
+}
